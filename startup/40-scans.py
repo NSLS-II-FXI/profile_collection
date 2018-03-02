e87f06b5be6eeb823a6b13dcfb2188f42487d9e9
FXI Operator <xf18id@xf18id-ws2.cs.nsls2.local>
2018-03-02 11:08:10 -0500

    Dan's updates for fly-scans with Andor

--- startup/40-scans.py
+++ startup/40-scans.py
@@ -1,10 +1,10 @@
 import uuid
 import sys
 import time
-from datetime import datetime
-from functools import wraps
 from warnings import warn
 from bluesky.plan_stubs import mv, mvr
+from ophyd.sim import motor1 as fake_motor
+from ophyd.sim import motor2 as fake_x_motor
 import subprocess
 import pandas as pd
 
@@ -28,7 +28,7 @@ def timeit(method):
 
 
 
-def tomo_scan(start, stop, num, exposure_time=0.1, detectors=[detA1], bkg_num=10, out_pos=1, md=None):
+def tomo_scan(start, stop, num, exposure_time=0.1, detectors=[detA1], bkg_num=10, dark_num=10, out_pos=1, note='', md=None):
     '''
     Script for running Tomography scan
     Use as: RE(tomo_scan(start, stop, num, exposure_time=0.1, detectors=[detA1], bkg_num=10, md=None))
@@ -40,91 +40,97 @@ def tomo_scan(start, stop, num, exposure_time=0.1, detectors=[detA1], bkg_num=10
     num: number of scan angles
     exposure time: second (default: 0.1)
     detectors: camera to capture image (default: detA1)
+    bkg_num: number of background image to be taken
+    dark_num: number of dark image to be taken
+    out_pos: position of pi_x stage where sample is out (absolute position: mm)
     md: metadate (default: None)
     '''
     yield from abs_set(detectors[0].cam.acquire_time, exposure_time)
     
-    motor_x = zps.pi_x
+    motor_x = phase_ring.x
+    # motor_x = zps.pi_x
     motor_x_ini = motor_x.position # initial position of motor_x
     motor_x_out = out_pos  # 'out position' of motor_x
         
-    motor_rot = zps.pi_r
+    # motor_rot = zps.pi_r
+    motor_rot = zps.sx
     motor_rot_ini = motor_rot.position  # initial position of motor_x
-
-    print('test')    
+    
     _md = {'detectors': [det.name for det in detectors],
            'motors': [motor_rot.name],
            'x_ray_energy': XEng.position,
            'num_angles': num,
            'num_bkg_images': bkg_num,
-           'num_dk_images': 0,
+           'num_dark_images': dark_num,
            'plan_args': {'detectors': list(map(repr, detectors)), 'num': num,
                          'motor': repr(motor_rot),
                          'start': start, 'stop': stop, 'exposure_time': exposure_time},
            'plan_name': 'tomo_scan',
            'plan_pattern': 'linspace',
            'plan_pattern_module': 'numpy',
-           'other_motor_position': {'zp_x': zp.x.position, 'zp_y': zp.y.position, 'zp_z': zp.z.position,
-                                    'aper_x': aper.x.position, 'aper_y': aper.y.position, 
-                                    'aper_z': aper.z.position , 'cond_x': clens.x.position,
-                                    'cond_y1': clens.y1.position, 'cond_y2': clens.y2.position, 
-                                    'cond_z1': clens.z1.position, 'cond_z2': clens.z2.position, 
-                                    'cond_pitch': clens.p.position,
-                                    'phase_ring_x': phase_ring.x.position, 'phase_ring_y': phase_ring.y.position,
-                                    'phase_ring_z': phase_ring.z.position, 'betr_x': betr.x.position,
-                                    'betr_y': betr.y.position, 'betr_z': betr.z.position,
-                                    'sample_x': zps.sx.position, 'sample_y': zps.sy.position,'sample_z': zps.sz.position, 
-                                    'sample_pi_x': zps.pi_x.position, 'sample_pi_rotate': zps.pi_r.position,
-                                    'DetU_x': DetU.x.position, 'DetU_y': DetU.y.position, 'DetU_z': DetU.z.position,
-                                    'dcm_eng': XEng.position, 'dcm_th1': dcm.th1.position, 'dcm_dy2': dcm.dy2.position,
-                                    'dcm_chi2': dcm.chi2.position},
            'hints': {},
+           'note': note,
            'operator': 'FXI'
             }
     _md.update(md or {})
 
-    try:
-        dimensions = [(motor_rot.hints['fields'], 'primary')]
-    except (AttributeError, KeyError):
-        pass
-    else:
-        _md['hints'].setdefault('dimensions', dimensions)
+    try:  dimensions = [(motor_rot.hints['fields'], 'primary')]
+    except (AttributeError, KeyError):    pass
+    else: _md['hints'].setdefault('dimensions', dimensions)
 
     steps = np.linspace(start, stop, num)
     
     @stage_decorator(list(detectors) + [motor_rot, motor_x])
     @run_decorator(md=_md)
-
     def tomo_inner_scan():
-        yield from mv_stage(motor_x, motor_x_out)    # move pi_x stage to motor_x_out
-        print (('\n\nTaking background images...\npi_x position: {0}').format(motor_x.position))
+
+        #close shutter, dark images
+        print('\nshutter closed, taking dark images...')
+        yield from abs_set(shutter_close, 1, wait=True)
+        yield from abs_set(shutter_close, 1, wait=True)
+        time.sleep(2)
+        for num in range(dark_num):   # close the shutter, and take 10(default) dark image when stage is at out position
+            yield from trigger_and_read(list(detectors) + [motor_rot])        
+
         
-        for num in range(bkg_num):    # take 10 background image when stage is at out position
-            yield from trigger_and_read(list(detectors) + [motor_rot])
-        
-        yield from mv_stage(motor_x, motor_x_ini)   # move pi_x stage back to motor_x_start
-        print (('\npi_x position: {0}\n\nstarting tomo_scan...').format(motor_x.position))
-     
+        # Open shutter, tomo images  
+#        yield from abs_set(shutter_open, 1, wait=True)
+#        yield from abs_set(shutter_open, 1, wait=True)
+        time.sleep(2)
+        print ('shutter opened, pi_x position: {0}\n\nstarting tomo_scan...'.format(motor_x.position))
+
         for step in steps:  # take tomography images
             yield from one_1d_step(detectors, motor_rot, step)
+#        yield from mv_stage(motor_rot, motor_rot_ini)
+        yield from mov(motor_rot, motor_rot_ini)
 
-        yield from mv_stage(motor_rot, motor_rot_ini)
+        # move out sample, bkg image
+#        yield from mv_stage(motor_x, motor_x_out)    # move pi_x stage to motor_x_out
+        print ('\n\nTaking background images...\npi_x position: {0}'.format(motor_x.position))
+        for num in range(bkg_num):    # take 10 background image when stage is at out position
+            yield from trigger_and_read(list(detectors) + [motor_rot])
 
+        # close shutter, move in the sample
+#        yield from mov(motor_x, motor_x_ini)   # move pi_x stage back to motor_x_start
+#        yield from abs_set(shutter_close, 1, wait=True)
+#        yield from abs_set(shutter_close, 1, wait=True)
     return (yield from tomo_inner_scan())
 
 
-def xanes_scan(eng_list, exposure_time=0.1, detectors=[detA1], bkg_num=10, out_pos=1, md=None):
 
+
+
+def xanes_scan(eng_list, exposure_time=0.1, detectors=[detA1], bkg_num=10, dark_num=10, out_pos=1, md=None):
     yield from mv(detectors[0].cam.acquire_time, exposure_time)
     motor = XEng
     motor_x = zps.pi_x
-    motor_x_ini = motor_x.position
-    
+    motor_x_ini = motor_x.position    
     _md = {'detectors': [det.name for det in detectors],
            'motors': [motor.name],
            'x_ray_energy': motor.position,
            'num_eng': len(eng_list),
            'num_bkg_images': bkg_num,
+           'num_dark_images': dark_num,
            'plan_args': {'detectors': list(map(repr, detectors)),
                          'motor': repr(motor),
                          'exposure_time': exposure_time},
@@ -132,21 +138,15 @@ def xanes_scan(eng_list, exposure_time=0.1, detectors=[detA1], bkg_num=10, out_p
            'hints': {},
            'operator': 'FXI'
             }
-    _md.update(md or {})
-    
-    try:
-        dimensions = [(motor.hints['fields'], 'primary')]
-    except (AttributeError, KeyError):
-        pass
-    else:
-        _md['hints'].setdefault('dimensions', dimensions)
+    _md.update(md or {})    
+    try:   dimensions = [(motor.hints['fields'], 'primary')]
+    except (AttributeError, KeyError):  pass
+    else:   _md['hints'].setdefault('dimensions', dimensions)
     
     @stage_decorator(list(detectors) + [motor, motor_x])
     @run_decorator(md=_md)
-
     def xanes_inner_scan():
-        print('\nstart xanes scan ... ')
-        
+        print('\nstart xanes scan ... ')        
         for eng in eng_list:
             yield from mv_stage(motor, eng)
             yield from trigger_and_read(list(detectors) + [motor])
@@ -157,7 +157,12 @@ def xanes_scan(eng_list, exposure_time=0.1, detectors=[detA1], bkg_num=10, out_p
             for i in range(bkg_num):
                 yield from mv_stage(motor, eng)
                 yield from trigger_and_read(list(detectors) + [motor])
-        yield from mv(motor_x, motor_x_ini)
+
+        print('\ntake dark image after xanes scan, {} per each energy...'.format(dark_num))
+        for i in range(dark_num):
+            yield from trigger_and_read(list(detectors) + [motor])
+
+        yield from mv(motor_x, motor_x_ini) # move sample stage back to orginal position
     return (yield from xanes_inner_scan())
 
 
@@ -168,34 +173,106 @@ def mv_stage(motor, pos):
         yield Msg('wait', None, group=grp)
 
 
-
-
-
 def eng_scan(eng_start, eng_end, steps, dwell_time=1.):
     '''
     eng_start, eng_end are in unit of eV !!
     '''
-    check_eng_range([eng_start, eng_end])
+    check_eng_range(eng_start, eng_end)
     set_ic_dwell_time(dwell_time=dwell_time)
-    yield from scan([ic3, ic4], XEng, eng_start/1000, eng_end/1000, steps)
+    yield from scan([ic1, ic2], XEng, eng_start/1000, eng_end/1000, steps)
     h = db[-1]
-    y0 = np.array(list(h.data(ic3.name)))
-    y1 = np.array(list(h.data(ic4.name)))
+    y0 = np.array(list(h.data(ic1.name)))
+    y1 = np.array(list(h.data(ic2.name)))
     
     r = y0/y1
     x = np.linspace(eng_start, eng_end, steps)
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
-    ax1.title.set_text('ratio of: {0}/{1}'.format(ic4.name, ic3.name))    
+    ax1.title.set_text('ratio of: {0}/{1}'.format(ic1.name, ic2.name))    
     ax1.plot(x, r, '.-')
 
     ax2 = fig.add_subplot(212)         
-    ax2.title.set_text('differential of: {0}/{1}'.format(ic4.name, ic3.name))
+    ax2.title.set_text('differential of: {0}/{1}'.format(ic1.name, ic2.name))
     r_dif = np.array([0] + list(np.diff(r)))
     ax2.plot(x, r_dif, '.-')
     fig.subplots_adjust(hspace=.5)
     plt.show()
 
+
+
+
+def fly_scan(exposure_time=0.1, relative_rot_angle = 1000, chunk_size=20, note='', md=None):
+    # motor_rot = zps.pi_r
+    motor_rot = zps.sx   
+    # motor_x = zps.pi_x
+    #motor_x = phase_ring.x
+    detectors = [Andor]
+    current_rot_angle = motor_rot.position
+    target_rot_angle = current_rot_angle + relative_rot_angle 
+
+    assert (exposure_time <= 0.2), "Exposure time is too long, not suitable to run fly-scan. \nScan aborted."   
+
+
+    _md = {'detectors': ['Andor'],
+           'motors': [motor_rot.name],
+           'x_ray_energy': XEng.position,
+           'plan_args': {'detectors': list(map(repr, detectors)), 
+                         'motor': repr(motor_rot),
+                         'exposure_time': exposure_time},
+           'plan_name': 'fly_scan',
+           'num_bkg_images': chunk_size,
+           'num_dark_images': chunk_size,
+           'chunk_size': chunk_size,
+           'plan_pattern': 'linspace',
+           'plan_pattern_module': 'numpy',
+           'hints': {},
+           'operator': 'FXI',
+           'note': note if note else 'None',
+           'motor_pos': wh_pos(),
+            }
+    _md.update(md or {})
+
+    try:  dimensions = [(motor_rot.hints['fields'], 'primary')]
+    except (AttributeError, KeyError):    pass
+    else: _md['hints'].setdefault('dimensions', dimensions)
+
+    yield from mv(Andor.cam.num_images, chunk_size)
+    yield from abs_set(detectors[0].cam.acquire_time, exposure_time, wait=True) 
+
+    @stage_decorator(list(detectors) + [motor_rot])
+    @bpp.monitor_during_decorator([motor_rot])
+    @run_decorator(md=_md)
+    def fly_inner_scan(): 
+
+        #close shutter, dark images: numer=chunk_size (e.g.20)
+        print('\nshutter closed, taking dark images...')
+        # yield from abs_set(shutter_close, 1, wait=True)
+        # yield from abs_set(shutter_close, 1, wait=True)        
+        yield from trigger_and_read(list(detectors)) 
+        
+        #open shutter, tomo_images   
+        # yield from abs_set(shutter_open, 1, wait=True)
+        # yield from abs_set(shutter_open, 1, wait=True)
+        print ('\nshutter opened, taking tomo images...')
+        status = yield from abs_set(motor_rot, target_rot_angle, wait=False)
+        while not status.done:
+            yield from trigger_and_read(list(detectors))   
+
+        #move out sample, taking bkg images
+        # yield from mv_stage(motor_x, motor_x_out)    # move pi_x stage to motor_x_out
+        print ('\nTaking background images...')
+        #print('pi_x position: {0}'.format(motor_x.position))
+        yield from trigger_and_read(list(detectors))
+
+
+        #move sample in       
+        # yield from mv_stage(motor_x, motor_x_ini)   # move pi_x stage back to motor_x_start
+        # yield from abs_set(shutter_close, 1, wait=True)
+        # yield from abs_set(shutter_close, 1, wait=True)  
+
+    uid = yield from fly_inner_scan()
+    print('scan finished')
+    return uid
 
 
 
@@ -366,8 +443,8 @@ def pzt_scan_multiple(moving_pzt, start, stop, steps, read_back_dev, record_dev,
     for num in range(repeat_num):
         print('\nscan #' + str(num))
         pzt_readout, motor_readout, signal_readout = pzt_scan(moving_pzt, start, stop, steps, read_back_dev, record_dev, delay_time=delay_time, overlay_flag=1)
-        col_x = col_x_prefix + ' #' + str(num)
-        col_y = col_y_prefix + ' #' + str(num)
+        col_x = col_x_prefix + ' #' + '{:2.1f}'.format(num)
+        col_y = col_y_prefix + ' #' + '{:2.1f}'.format(num)
         df[col_x] = pd.Series(motor_readout)
         df[col_y] = pd.Series(signal_readout)
         time.sleep(1)
@@ -375,8 +452,8 @@ def pzt_scan_multiple(moving_pzt, start, stop, steps, read_back_dev, record_dev,
 
     fig = plt.figure()
     for num in range(repeat_num):
-        col_x = col_x_prefix + ' #' + str(num)
-        col_y = col_y_prefix + ' #' + str(num)
+        col_x = col_x_prefix + ' #' + '{:2.1f}'.format(num)
+        col_y = col_y_prefix + ' #' + '{:2.1f}'.format(num)
         x, y = df[col_x], df[col_y]
         plt.plot(x, y)
     plt.title('X-ray Energy: {:2.1f}keV'.format(current_eng))
@@ -402,22 +479,23 @@ def pzt_overnigh_scan():
     shutter_open.put(1)
     time.sleep(5)
     print('shutter open')
-    engs = np.arange(5, 13, 0.5)
+    engs = np.arange(7, 13.5, 0.5)
     for eng in engs:
         RE(abs_set(XEng, eng))
-        time.sleep(60)
+        time.sleep(30)
         current_eng = XEng.position
         print('current X-ray Energy: {:2.1f}keV'.format(current_eng))
         pzt_scan_multiple(pzt_dcm_th2, -18, 18, 73, dcm.th2, Vout2, repeat_num=1)
-
+    
     RE(abs_set(XEng, 9))
     time.sleep(300)
     current_eng = XEng.position
     print('current X-ray Energy: {:2.1f}keV'.format(current_eng))
     print('run 10 times each 1-hour')
-    for i in range(12):
+    for i in range(16):
         pzt_scan_multiple(pzt_dcm_th2, -18, 18, 73, dcm.th2, Vout2, repeat_num=10)
-
+        time.sleep(3600)
+    
     shutter_close.put(1)    
         
 
