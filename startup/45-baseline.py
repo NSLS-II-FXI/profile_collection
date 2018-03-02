5209d3721ea805210e35a4faba78ca6f04622930
FXI Operator <xf18id@xf18id-ws2.cs.nsls2.local>
2018-03-02 10:45:59 -0500

    Added baseline readings config

---
+++ startup/45-baseline.py
@@ -0,0 +1,16 @@
+# Certain things to save read at the beginning and end of every run and recorded# just in case.
+sd.baseline = [
+    zp,
+    aper,
+    clens,
+    phase_ring,
+    betr,
+    # zps,
+    DetU,
+    XEng,
+    dcm,
+    cm,
+    tm,
+    pbsl,
+]
+bec.disable_baseline()
