2ce4320da38f9549b5aeb53a81c944ada29c9808
FXI Operator <xf18id@xf18id-ws2.cs.nsls2.local>
2018-03-02 11:04:57 -0500

    Added config of Andor det

--- startup/10-area-detector.py
+++ startup/10-area-detector.py
@@ -20,15 +20,15 @@ class HDF5PluginWithFileStore(HDF5Plugin, FileStoreHDF5IterativeWrite):
 class Manta(SingleTrigger, AreaDetector):
     image = Cpt(ImagePlugin, 'image1:')
     stats1 = Cpt(StatsPlugin, 'Stats1:')
-    stats2 = Cpt(StatsPlugin, 'Stats2:')
+#    stats2 = Cpt(StatsPlugin, 'Stats2:')
 #    stats3 = Cpt(StatsPlugin, 'Stats3:')
-    stats4 = Cpt(StatsPlugin, 'Stats4:')
-    stats5 = Cpt(StatsPlugin, 'Stats5:')
+#    stats4 = Cpt(StatsPlugin, 'Stats4:')
+#    stats5 = Cpt(StatsPlugin, 'Stats5:')
     trans1 = Cpt(TransformPlugin, 'Trans1:')
     roi1 = Cpt(ROIPlugin, 'ROI1:')
-    roi2 = Cpt(ROIPlugin, 'ROI2:')
-    roi3 = Cpt(ROIPlugin, 'ROI3:')
-    roi4 = Cpt(ROIPlugin, 'ROI4:')
+#    roi2 = Cpt(ROIPlugin, 'ROI2:')
+#    roi3 = Cpt(ROIPlugin, 'ROI3:')
+#    roi4 = Cpt(ROIPlugin, 'ROI4:')
     proc1 = Cpt(ProcessPlugin, 'Proc1:')
 
     hdf5 = Cpt(HDF5PluginWithFileStore,
@@ -58,10 +58,11 @@ class Manta(SingleTrigger, AreaDetector):
 detA1 = Manta('XF:18IDB-BI{Det:A1}', name='detA1')
 detA1.hdf5.reg = db.reg
 detA1.hdf5._reg = db.reg
-detA1.read_attrs = ['hdf5', 'stats1', 'stats5']
-#detA1.read_attrs = ['hdf5', 'stats1']
+#detA1.read_attrs = ['hdf5', 'stats1', 'stats5']
+#detA1.read_attrs = ['hdf5']
+detA1.read_attrs = ['hdf5', 'stats1']
 detA1.stats1.read_attrs = ['total']
-detA1.stats5.read_attrs = ['total']
+#detA1.stats5.read_attrs = ['total']
 detA1.hdf5.read_attrs = []
 
 
@@ -69,9 +70,10 @@ detA1.hdf5.read_attrs = []
 Andor = Manta('XF:18IDB-BI{Det:Neo}', name = 'Andor')
 Andor.hdf5.reg = db.reg
 Andor.hdf5._reg = db.reg
-Andor.read_attrs = ['hdf5', 'stats1', 'stats5']
-#Andor.read_attrs = ['hdf5', 'stats1']
+#Andor.read_attrs = ['hdf5', 'stats1', 'stats5']
+#Andor.read_attrs = ['hdf5']
+Andor.read_attrs = ['hdf5', 'stats1']
 Andor.stats1.read_attrs = ['total']
-Andor.stats5.read_attrs = ['total']
+#Andor.stats5.read_attrs = ['total']
 Andor.hdf5.read_attrs = []
 Andor.stage_sigs['cam.image_mode'] = 0
