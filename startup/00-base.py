ba5f62f6f2b794a11294ade96749b1c7df1405fc
FXI Operator <xf18id@xf18id-ws2.cs.nsls2.local>
2020-08-24 13:55:33 -0400

    Update to use new metadata storage for RE.md.

--- startup/00-base.py
+++ startup/00-base.py
@@ -10,6 +10,59 @@ from bluesky.preprocessors import stage_decorator, run_decorator
 
 nslsii.configure_base(get_ipython().user_ns, "fxi", bec=True)
 
+# Work around ophyd being too noisy in 1.5.2
+import logging
+logging.getLogger('ophyd').setLevel('WARNING')
+
+
+# Make new RE.md storage available in old environments.
+from pathlib import Path
+
+import appdirs
+
+
+try:
+    from bluesky.utils import PersistentDict
+except ImportError:
+    import msgpack
+    import msgpack_numpy
+    import zict
+
+    class PersistentDict(zict.Func):
+        def __init__(self, directory):
+            self._directory = directory
+            self._file = zict.File(directory)
+            super().__init__(self._dump, self._load, self._file)
+
+        @property
+        def directory(self):
+            return self._directory
+
+        def __repr__(self):
+            return f"<{self.__class__.__name__} {dict(self)!r}>"
+
+        @staticmethod
+        def _dump(obj):
+            "Encode as msgpack using numpy-aware encoder."
+            # See https://github.com/msgpack/msgpack-python#string-and-binary-type
+            # for more on use_bin_type.
+            return msgpack.packb(
+                obj,
+                default=msgpack_numpy.encode,
+                use_bin_type=True)
+
+        @staticmethod
+        def _load(file):
+            return msgpack.unpackb(
+                file,
+                object_hook=msgpack_numpy.decode,
+                raw=False)
+
+runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
+
+# PersistentDict will create the directory if it does not exist
+RE.md = PersistentDict(runengine_metadata_dir)
+
 # disable plotting from best effort callback
 bec.disable_plots()
 
