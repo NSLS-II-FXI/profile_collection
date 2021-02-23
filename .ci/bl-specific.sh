d7ef59f153b65ee45b92b80db5d275b7b198722c
Abigail Giles <agiles@bnl.gov>
2021-02-23 13:20:54 -0500

    Added azure testing conditional

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+export AZURE_TESTING=1
+
+sudo mkdir -v -p /NSLS2/xf18id1/DATA/FXI_log/
+sudo chown -rv $USER: /NSLS2
+touch /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
+
--- startup/92-run_function_at_start.py
+++ startup/92-run_function_at_start.py
@@ -1,4 +1,6 @@
-new_user()
+import os
+if not os.environ.get('AZURE_TESTING'):
+    new_user()
 show_global_para()
 run_pdf()
 read_calib_file_new() # read file from: /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
