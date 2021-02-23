67e2770e8853283fbe6985817125aaec96c56f82
Maksim Rakitin <mrakitin@bnl.gov>
2021-02-23 14:26:42 -0500

    Create required dirs/files dynamically

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -8,3 +8,10 @@ sudo chown -Rv $USER: /NSLS2
 echo -e "Current directory: $PWD"
 
 cp -v .ci/calib_new.csv /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
+
+files_to_touch=$(grep '/NSLS2/' startup/99-umacro.py | cut -d\" -f 2 | grep '\.txt$')
+
+for f in $files_to_touch; do
+    mkdir -v -p $(dirname $f)
+    touch $f
+done
