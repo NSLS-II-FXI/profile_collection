f288a9f5f72968420d00adc3c3765134d227500a
Maksim Rakitin <mrakitin@bnl.gov>
2021-02-23 13:43:10 -0500

    Add calib_new.csv file for CI only

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -4,5 +4,7 @@ export AZURE_TESTING=1
 
 sudo mkdir -v -p /NSLS2/xf18id1/DATA/FXI_log/
 sudo chown -Rv $USER: /NSLS2
-touch /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
 
+echo -e "Current directory: $PWD"
+
+cp -v .ci/calib_new.csv /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
