7e304dcd3dea1d555ffca6d036901ed3dbf888a4
Abigail Giles <agiles@bnl.gov>
2021-02-23 13:38:45 -0500

    Fix typo

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -3,6 +3,6 @@
 export AZURE_TESTING=1
 
 sudo mkdir -v -p /NSLS2/xf18id1/DATA/FXI_log/
-sudo chown -rv $USER: /NSLS2
+sudo chown -Rv $USER: /NSLS2
 touch /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
 
