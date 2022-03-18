f9ff467d7a644687eef1065b0fb732d89b6c3ac3
Thomas A Caswell <tcaswell@bnl.gov>
2022-03-18 14:22:39 -0400

    CI: update what files to make available

--- .ci/bl-specific.sh
+++ .ci/bl-specific.sh
@@ -4,12 +4,13 @@ export AZURE_TESTING=1
 
 sudo mkdir -v -p /NSLS2/xf18id1/DATA/FXI_log/
 sudo chown -Rv $USER: /NSLS2
+sudo chown -Rv $USER: /nsls2/data
 
 echo -e "Current directory: $PWD"
 
 cp -v .ci/calib_new.csv /NSLS2/xf18id1/DATA/FXI_log/calib_new.csv
 
-files_to_touch=$(grep '/NSLS2/' startup/99-umacro.py | cut -d\" -f 2 | grep '\.txt$')
+files_to_touch=$(grep '/nsls2/data/' startup/99-umacro.py | cut -d\" -f 2 | grep '\.txt$')
 
 for f in $files_to_touch; do
     mkdir -v -p $(dirname $f)
